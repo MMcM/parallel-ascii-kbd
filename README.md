0d6ab96ae0fbe4595275e200aecf3874525bb6ae
Mike McMahon <mmcm@comcast.net>
2015-06-07 01:40:13 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+# Micro Switch ASCII Keyboard USB driver #
+
+A virtual serial port driver for a Micro Switch 7-bit ASCII keyboard.
+
+The hardware documentation that came with this keyboard can be found
+[here](http://home.comcast.net/~mmcm/kbd/Micro%20Switch.PDF).
+
+This keyboard is mostly the same as the
+[NCR one](http://www.computerhistory.org/collections/catalog/102672973)
+in the Computer History Museum, except it has &Ntilde; instead of
+semicolon and a few other characters moved as a result.
+
+The state of the control and shift keys is also available separately,
+so it would be possible to recover more of the key state from the
+7-bit character. But, really, there aren't enough keys for this to be
+a modern keyboard.
+
+## Hardware ##
+
+The board needs a lot of GPIO pins, so an actual Arduino is not a great choice.
+
+* [Teensy 2.0](https://www.pjrc.com/teensy/index.html).
+* [Adafruit Atmega32u4 breakout](http://www.ladyada.net/products/atmega32u4breakout/).
+
+The microcontroller is labeled SW-20306 and is, I believe, a masked ROM version
+of some AMI 4-bit MCU from the 70s. It is CMOS and Vcc is -12V DC. Vdd is +5V
+DC, for the control signals, both inputs from the Hall Effect switches and
+character output. A DC-DC isolating converter like the
+[Mornsun](http://www.mornsun-power.com/public/pdf/DCDC/B_D-1W.pdf) B0512S-1W
+seems to work fine.
+
+### Connections ###
+
+| PCB | Signal              | AVR |
+|-----|---------------------|-----|
+| 2   | -12V                |     |
+| 3   | +5V                 | +5V |
+| 4   | GND                 | GND |
+| 10  | CONTROL             | PD3 |
+| 12  | SHIFT 	            | PD1 |
+|     |                     |     |
+| A   | STROBE              | PD0 |
+| C   | +5V                 | +5V |
+| D   | GND                 | GND |
+| F   | CHAR BIT 2          | PB1 |
+| H   | CHAR BIT 1          | PB0 |
+| J   | CHAR BIT 4          | PB3 |
+| K   | CHAR BIT 3          | PB2 |
+| L   | CHAR BIT 5          | PB4 |
+| M   | CHAR BIT 6          | PB5 |
+| N   | CHAR BIT 7          | PB6 |
---
+++ src/VirtualSerial.c
@@ -0,0 +1,241 @@
+/*
+  Copyright 2015 Mike McMahon
+
+             LUFA Library
+     Copyright (C) Dean Camera, 2014.
+
+  dean [at] fourwalledcubicle [dot] com
+           www.lufa-lib.org
+*/
+
+/*
+  Copyright 2014  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting
+  documentation, and that the name of the author not be used in
+  advertising or publicity pertaining to distribution of the
+  software without specific, written prior permission.
+
+  The author disclaims all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  Main source file for the VirtualSerial demo. This file contains the main tasks of
+ *  the demo and is responsible for the initial application hardware configuration.
+ */
+
+#include "VirtualSerial.h"
+
+/** LUFA CDC Class driver interface configuration and state information. This structure is
+ *  passed to all CDC Class driver functions, so that multiple instances of the same class
+ *  within a device can be differentiated from one another.
+ */
+USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface =
+  {
+    .Config =
+      {
+        .ControlInterfaceNumber   = INTERFACE_ID_CDC_CCI,
+        .DataINEndpoint           =
+          {
+            .Address          = CDC_TX_EPADDR,
+            .Size             = CDC_TXRX_EPSIZE,
+            .Banks            = 1,
+          },
+        .DataOUTEndpoint =
+          {
+            .Address          = CDC_RX_EPADDR,
+            .Size             = CDC_TXRX_EPSIZE,
+            .Banks            = 1,
+          },
+        .NotificationEndpoint =
+          {
+            .Address          = CDC_NOTIFICATION_EPADDR,
+            .Size             = CDC_NOTIFICATION_EPSIZE,
+            .Banks            = 1,
+          },
+      },
+  };
+
+#define CHAR_PORT PORTB
+#define CHAR_PIN PINB
+
+#define CONTROL_PORT PORTD
+#define CONTROL_PIN PIND
+#define CONTROL_DDR DDRD
+#define CONTROL_STROBE (1 << 0)
+#define CONTROL_SHIFT (1 << 1)
+#define CONTROL_CTRL (1 << 3)
+
+/*** Character Queue ***/
+
+typedef struct {
+  unsigned charCode:7;
+  unsigned shift:1;
+  unsigned ctrl:1;
+} queue_entry_t;
+#define QUEUE_SIZE 16
+static queue_entry_t CharQueue[QUEUE_SIZE];
+static uint8_t CharQueueIn, CharQueueOut;
+
+static inline void QueueClear(void)
+{
+  CharQueueIn = CharQueueOut = 0;
+}
+
+static inline bool QueueIsEmpty(void)
+{
+  return (CharQueueIn == CharQueueOut);
+}
+
+static inline bool QueueIsFull(void)
+{
+  // One entry wasted to be able to check this easily.
+  return (((CharQueueIn + 1) % QUEUE_SIZE) == CharQueueOut);
+}
+
+static inline queue_entry_t QueueRemove(void)
+{
+  queue_entry_t entry = CharQueue[CharQueueOut];
+  CharQueueOut = (CharQueueOut + 1) % QUEUE_SIZE;
+  return entry;
+}
+
+static inline void QueueAdd(queue_entry_t entry)
+{
+  CharQueue[CharQueueIn] = entry;
+  CharQueueIn = (CharQueueIn + 1) % QUEUE_SIZE;
+}
+
+/*** Keyboard Interface ***/
+
+static void Micro_Switch_Kbd_Init(void)
+{
+  QueueClear();
+
+  // Enable pullups.
+  CHAR_PORT |= 0x7F;
+  CONTROL_PORT |= CONTROL_STROBE | CONTROL_SHIFT | CONTROL_CTRL;
+
+  // Interrupt 0 on falling edge of STROBE
+  EIMSK |= (1 << INT0);
+  EICRA |= (1 << ISC01);
+}
+
+static void Micro_Switch_Kbd_Task(void)
+{
+  queue_entry_t entry;
+  
+  // NOTE: If simulating a full keyboard and not a 7-bit serial port, it
+  // would be possible to use the shift and ctrl bits to reconstruct more
+  // of the key state.
+
+  while (!QueueIsEmpty()) {
+    entry = QueueRemove();
+    CDC_Device_SendByte(&VirtualSerial_CDC_Interface, entry.charCode);
+  }
+}
+
+/*** Interrupt Handler ***/
+
+ISR(INT0_vect)
+{
+  queue_entry_t entry;
+
+  entry.charCode = CHAR_PIN & 0x7F;
+  entry.shift = ((CONTROL_PIN & CONTROL_SHIFT) == 0);
+  entry.ctrl = ((CONTROL_PIN & CONTROL_CTRL) == 0);
+
+  if (!QueueIsFull()) {
+    QueueAdd(entry);
+  }
+}
+
+/** Main program entry point. This routine contains the overall program flow, including initial
+ *  setup of all components and the main program loop.
+ */
+int main(void)
+{
+  SetupHardware();
+
+  LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+  GlobalInterruptEnable();
+
+  for (;;)
+  {
+    Micro_Switch_Kbd_Task();
+
+    /* Must throw away unused bytes from the host, or it will lock up while waiting for the device */
+    CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
+
+    CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
+    USB_USBTask();
+  }
+}
+
+/** Configures the board hardware and chip peripherals for the demo's functionality. */
+void SetupHardware(void)
+{
+#if (ARCH == ARCH_AVR8)
+  /* Disable watchdog if enabled by bootloader/fuses */
+  MCUSR &= ~(1 << WDRF);
+  wdt_disable();
+
+  /* Disable clock division */
+  clock_prescale_set(clock_div_1);
+#elif (ARCH == ARCH_XMEGA)
+  /* Start the PLL to multiply the 2MHz RC oscillator to 32MHz and switch the CPU core to run from it */
+  XMEGACLK_StartPLL(CLOCK_SRC_INT_RC2MHZ, 2000000, F_CPU);
+  XMEGACLK_SetCPUClockSource(CLOCK_SRC_PLL);
+
+  /* Start the 32MHz internal RC oscillator and start the DFLL to increase it to 48MHz using the USB SOF as a reference */
+  XMEGACLK_StartInternalOscillator(CLOCK_SRC_INT_RC32MHZ);
+  XMEGACLK_StartDFLL(CLOCK_SRC_INT_RC32MHZ, DFLL_REF_INT_USBSOF, F_USB);
+
+  PMIC.CTRL = PMIC_LOLVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm;
+#endif
+
+  /* Hardware Initialization */
+  Micro_Switch_Kbd_Init();
+  LEDs_Init();
+  USB_Init();
+}
+
+/** Event handler for the library USB Connection event. */
+void EVENT_USB_Device_Connect(void)
+{
+  LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
+}
+
+/** Event handler for the library USB Disconnection event. */
+void EVENT_USB_Device_Disconnect(void)
+{
+  LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+}
+
+/** Event handler for the library USB Configuration Changed event. */
+void EVENT_USB_Device_ConfigurationChanged(void)
+{
+  bool ConfigSuccess = true;
+
+  ConfigSuccess &= CDC_Device_ConfigureEndpoints(&VirtualSerial_CDC_Interface);
+
+  LEDs_SetAllLEDs(ConfigSuccess ? LEDMASK_USB_READY : LEDMASK_USB_ERROR);
+}
+
+/** Event handler for the library USB Control Request reception event. */
+void EVENT_USB_Device_ControlRequest(void)
+{
+  CDC_Device_ProcessControlRequest(&VirtualSerial_CDC_Interface);
+}
